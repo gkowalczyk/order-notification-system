83f3c667d685ae82ae92d76d55750adfe0d3500e
Grzegorz Kowalczyk <grzegorzkowalczyk1989@gmail.com>
2026-02-25 17:00:20 +0100

    feat: add audit entity and repository

--- src/main/java/com/example/ordernotificationsystem/entity/OrderEventAuditRepository.java
+++ src/main/java/com/example/ordernotificationsystem/entity/OrderEventAuditRepository.java
@@ -1,4 +1,10 @@
 package com.example.ordernotificationsystem.entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OrderEventAuditRepository {
+import java.util.UUID;
+
+@Repository
+public interface OrderEventAuditRepository
+extends JpaRepository<OrderEventAudit, UUID> {
 }
